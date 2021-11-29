5f43926f79fec37ec869b31bb04b716c4a130cbc
tzagkaris <csd4279@csd.uoc.gr>
2021-11-29 22:01:43 +0200

    Added template

---
+++ fullstack-template/backend/src/api/v1/index.ts
@@ -0,0 +1,32 @@
+import * as express from 'express';
+import { ResourceController } from '../shared';
+import { ITask, TaskModel } from '@app/models';
+import { FilesController } from './files/files.controller';
+import { SocketEventsController } from './socket-events/socket-events.controller';
+
+
+const apiV1Router = express.Router();
+
+
+apiV1Router
+  // Sockets events routes
+  .use(
+    '/socket-events',
+    new SocketEventsController().applyRoutes()
+  )
+
+  // Sockets events routes
+  .use(
+    '/files',
+    new FilesController().applyRoutes()
+  )
+
+  // Task routes
+  .use(
+    '/tasks',
+    new ResourceController<ITask>(TaskModel).applyRoutes()
+  )
+
+
+export { apiV1Router };
+
